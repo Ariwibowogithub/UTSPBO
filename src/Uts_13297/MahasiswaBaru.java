5c86b3659933860e30cad3f3795ce2c2d9e49992
Ariwibowogithub <arie35411@gmai.com>
2023-05-12 08:57:00 +0700

    first commit

---
+++ src/Uts_13297/MahasiswaBaru.java
@@ -0,0 +1,19 @@
+package Uts_13297;
+
+class MahasiswaBaru extends Mahasiswa {
+    String asalSekolah;
+
+    public MahasiswaBaru(String nim, String nama, int semester, int usia, String[] krs, String asalSekolah) {
+        super(nim, nama, semester, usia, krs);
+        this.asalSekolah = asalSekolah;
+    }
+
+    public boolean ikutOspek() {
+        return true;
+    }
+
+    public void infoMahasiswa() {
+        super.infoMahasiswa();
+        System.out.println("Asal Sekolah : " + this.asalSekolah);
+    }
+}
